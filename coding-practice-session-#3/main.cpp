ec08e7ceafad5e55ec85b75e00755ff5066a987d
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-08-29 20:25:43 +0300

    Added main.cpp file

---
+++ coding-practice-session-#3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+//#include "SampleProblem.h"
+//#include "RecordBreaker.h"
+//#include "WiggleWalk.h"
+//#include "GBusCount.h"
+#include "SherlockAndWatsonGymSecrets.h"
+
+using namespace std;
+
+int main() {
+
+//    cout << solve_sample_problem() << '\n';
+//    cout << solve_reacord_breaker() << '\n';
+//    solve_wiggle_walk();
+//    solve_gbus_count();
+    solve_gym_secrets();
+    return 0;
+}
